81fa0515b166f369c1f34b355ebf3343c5ba7d71
Akash <akashgupta515@gmail.com>
2021-06-01 03:45:51 +0530

    Canvas toolbar

--- src/Components/PaintComponents/PaintComponent.jsx
+++ src/Components/PaintComponents/PaintComponent.jsx
@@ -26,11 +26,20 @@ export default function PaintComponent() {
         CanvasRef.current.width = parseInt(DrawingWidth);
         CanvasRef.current.height = parseInt(DrawingHeight);
         ctx = CanvasRef.current.getContext("2d")
-        ctx.lineWidth = 1;
+        ctx.lineWidth = 25;
         ctx.lineJoin = 'round';
         ctx.lineCap = 'round';
-        ctx.strokeStyle = "#2b4967";
+        ctx.strokeStyle = "#000";
         
+    }
+    const ChangeColorStroke = (color) =>{
+            ctx.strokeStyle = color
+    }
+    const ClearCanvas = () =>{
+        ctx.clearRect(0, 0, CanvasRef.current.width, CanvasRef.current.height);
+    }
+    const ChangeStrokeSize = (size) =>{
+        ctx.lineWidth = size
     }
     const CaptureMouseDown = (event) =>{
         //console.log(event.target)
@@ -65,11 +74,11 @@ export default function PaintComponent() {
         <div className="container-fluid" style={{height:"100%",backgroundColor:"white"}}>
             <div className="row" id="Board" >
                 <div style={{border:"1px solid red",height:"85vh"}}  className="col-7 p-0">
+                <div className="p-0" style={{border:"1px solid pink",height:"10vh"}}>
+                        <ToolBarComponent ClearCanvas={ClearCanvas} changeColor={ChangeColorStroke} Changestroke={ChangeStrokeSize}/>
+                </div>
                     <div ref={DrawingBoardRef} className="p-0" style={{border:"1px solid green",height:"75vh"}}>
                         <canvas ref={CanvasRef} onMouseUp={CaptureMouseUp} onMouseMove={onMouseMoveCanvas} onMouseDown={CaptureMouseDown}></canvas>
-                    </div>
-                    <div className="p-0" style={{border:"1px solid pink",height:"10vh"}}>
-                        <ToolBarComponent/>
                     </div>                                
                 </div>
                 <div style={{border:"1px solid black",height:"85vh"}} className="col-5">
--- src/Components/PaintComponents/ToolBarComponent.jsx
+++ src/Components/PaintComponents/ToolBarComponent.jsx
@@ -1,21 +1,72 @@
 import {React,useState} from 'react'
 import { ChromePicker } from 'react-color'
-export default function ToolBarComponent({changeColor,Changestroke,IsEraser,ClearCanvas}){
+export default function ToolBarComponent({changeColor,Changestroke,ClearCanvas}){
 
-      const [background, setbackground] = useState("#fff")
-
+      const [background, setbackground] = useState("#000")
+      const [ShowColorPicker,hidePicker] = useState(false)
+      const [strokeSize,SetStrokeSize] = useState(1)
+      let stroke
+      const ShowColorPickerOnClick = (event) =>{
+        if(ShowColorPicker){
+          console.log("click")
+          hidePicker(false)
+        }
+        else{
+          hidePicker(true)
+        }
+      }
       const handleChangeComplete = (color) => {
         //this.setState({ background: color.hex });
         setbackground(color.hex);
+        changeColor(color.hex)
       };
+      const HandleStrokeSizeChange = (event) =>{
+        console.log(event.target.value)
+        SetStrokeSize(event.target.value)
+        Changestroke(event.target.value)
+      }
+      const ActivateEraser = event =>{
+        changeColor("#ffffff")
+      }
     
     return (
-        <div className="container-fluid p-0">
-            <div className="row" style={{border:"2px solid blue"}} style={{width:"50%"}}> 
-                    <ChromePicker
+        <div className="container-fluid p-2" style={{border:"1px solid black",height:"100%"}}>
+          <div className="row p-0 m-0" style={{width:"100%",height:"100%"}}>
+          <div className="row justify-content-center align-items-center p-0 m-0 col-3" style={{height:"100%"}}>
+                <div className="col-5" style={{height:"100%"}}>
+                  color
+                </div>
+                <div onClick={ShowColorPickerOnClick} className="col-7" style={{ height:"100%",backgroundColor:background}}>
+                {ShowColorPicker ? <div style={{position:"absolute"}}>
+                  <ChromePicker
                     color={ background }
-                    onChangeComplete={ handleChangeComplete }/>
+                    onChange={ handleChangeComplete }/>
+                </div> : null} 
+                </div>
             </div>
+            <div className="col-5 row justify-content-center align-items-center p-0 m-0" style={{height:"100%"}}>
+              <div className="col-6">
+                <label>Stroke size</label>
+                <input type="range" onChange={HandleStrokeSizeChange} min="1" className="form-range" max="25"/>
+              </div>
+              <div className="col-4 d-flex justify-content-center align-items-center" style={{border:"1px solid black",height:"100%"}} >
+                      <div className="rounded-circle" style={{height:`${strokeSize}px`,width:`${strokeSize}px`,backgroundColor:background}}>
+                      </div>
+              </div>
+            </div>
+            <div className="col row align-items-center m-0 p-0">
+                  <div className="col row m-0 p-0 justify-content-center align-items-center" style={{height:"100%"}}>
+                      <div>
+                        <button onClick={ActivateEraser} type="button" class="btn btn-primary">Eraser</button>
+                      </div>
+                  </div>
+                  <div className="col row m-0 p-0 justify-content-center align-items-center" style={{height:"100%"}}>
+                      <div>
+                        <button onClick={ (event) => ClearCanvas()} type="button" class="btn btn-primary">clear</button>
+                      </div>
+                  </div>
+            </div>
+          </div>
         </div>
     )
 }
